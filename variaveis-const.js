a73d9be2d27293585f98d1239e3b0ace6749f4ee
Joe <joedsonfera@hotmail.com>
2023-04-10 18:40:51 -0300

    third commit

---
+++ variaveis-const.js
@@ -0,0 +1,12 @@
+// atribuindo um valor a uma variável "const";
+// atribuindo vários valores a uma variável "let";
+
+let primeiroNome = "Joe";
+
+console.log(primeiroNome);
+
+console.log("Abacaxi");
+
+primeiroNome = "Pedro";
+
+console.log(primeiroNome);
