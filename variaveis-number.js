bfc9a2a9cc598a4ddbbe6a0cb1ab2194c967a42e
Joe <joedsonfera@hotmail.com>
2023-04-10 18:54:31 -0300

    fifht commit

---
+++ variaveis-number.js
@@ -0,0 +1,8 @@
+//"é convencionado que quando se cria variáveis se comece com letras minúsculas"
+
+let idade = 31;
+console.log(idade);
+
+//camelCase = padrão em que a cada mudança de palavra se coloca uma letra maíuscula
+let alturaEmMetros = 1.75;
+console.log(alturaEmMetros);
