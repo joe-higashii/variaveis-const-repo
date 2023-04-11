ec26a3adf90e9ae1b4dd2f6359c20e3ba69bfb49
Joe <joedsonfera@hotmail.com>
2023-04-10 22:12:58 -0300

    tenth commit

--- variaveis-aritmeticos.js
+++ variaveis-aritmeticos.js
@@ -3,15 +3,16 @@ let x = 10, y = 2;
 let resultado = x + y;
 
 console.log(resultado); //12
+console.log(`A soma de ${x} e ${y} é ${resultado}`);
 
 resultado = x - y;
-
 console.log(resultado); //8
+console.log(`A subtração entre ${x} e ${y} é ${resultado}`);
 
 resultado = x * y;
-
 console.log(resultado); //20
+console.log(`A multiplicação de ${x} e ${y} é ${resultado}`);
 
 resultado = x / y;
-
 console.log(resultado); //5
+console.log(`A divisão entre ${x} e ${y} é ${resultado}`);
---
+++ variaveis-concatenacao.js
@@ -0,0 +1,7 @@
+//concatenação de strings
+let nome = "joe";
+let sobrenome = "mendes";
+
+let nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
