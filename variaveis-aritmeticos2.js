b65a0dbe159a3fa3ae9878b6206aabe40ded2b1b
Joe <joedsonfera@hotmail.com>
2023-04-10 23:05:41 -0300

    fifteenth commit

---
+++ variaveis-aritmeticos2.js
@@ -0,0 +1,15 @@
+let x = 12;
+
+// x = x + 3;
+// x += 3; //15 (incremente ao próprio x o valor 3)
+
+// x = x - 3; //9
+// x -= 3; //9
+
+//x = x * 3; //36
+//x *= 3; //36
+
+x = x / 3; //4
+//x /= 3; //4
+
+console.log(x);
