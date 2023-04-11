8b20eb551a9eb66cf20420252ef25f22c8bc91de
Joe <joedsonfera@hotmail.com>
2023-04-10 21:45:57 -0300

    eight commit

---
+++ variaveis-tipos.js
@@ -0,0 +1,11 @@
+let nome = "Joe"; //string
+let idade = 31; //number
+let altura = 1.75; //number
+
+let nota;
+
+console.log(nota);
+nota = 10;
+console.log(nota);
+nota = null;
+console.log(nota);
