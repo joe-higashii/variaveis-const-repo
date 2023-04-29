62491ff907304c3ab51012cebd19a92f90b074e1
Joe <joedsonfera@hotmail.com>
2023-04-29 20:59:17 -0300

    first commit of the day

---
+++ index.js
@@ -0,0 +1,9 @@
+const numeroDeLados = 6;
+
+const somaDosLados = (numeroDeLados - 2) * 180;
+
+console.log(`A soma dos angulos internos é ${somaDosLados}`);
+
+const anguloInterno = somaDosLados / numeroDeLados;
+
+console.log(`Caso os angulos sejam iguais cada um deles vale ${anguloInterno}`);
