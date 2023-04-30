99912091862cf7cedc5b11f640669f4521dbf16f
Joe <joedsonfera@hotmail.com>
2023-04-29 23:01:26 -0300

    third commit of the day

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// const numeroDeLados = 6;
+
+// const somaDosLados = (numeroDeLados - 2) * 180;
+
+// console.log(`A soma dos angulos internos é ${somaDosLados}`);
+
+// const anguloInterno = somaDosLados / numeroDeLados;
+
+// console.log(`Caso os angulos sejam iguais cada um deles vale ${anguloInterno}`);
+
 const numeroDeLados = 6;
 
 const somaDosLados = (numeroDeLados - 2) * 180;
 
-console.log(`A soma dos angulos internos é ${somaDosLados}`);
+console.log(somaDosLados);
 
 const anguloInterno = somaDosLados / numeroDeLados;
 
-console.log(`Caso os angulos sejam iguais cada um deles vale ${anguloInterno}`);
+console.log(anguloInterno);
